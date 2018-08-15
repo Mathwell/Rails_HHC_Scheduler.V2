af99921a7d943516fe9d4f146e69096107e93542
Olena Ageyeva <eageeva@hotmail.com>
2018-08-15 23:49:16 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         session[:user_id] = @user.id
-        format.html { redirect_to user_path(@user), notice: "Welcome to the theme park!" }
+        format.html { redirect_to user_path(@user), notice: "Welcome to HHH Scheduler!" }
       else
         format.html { render :new }
       end
