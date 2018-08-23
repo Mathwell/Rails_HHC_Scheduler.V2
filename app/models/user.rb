bdcfb2f42dca2ef83995570a57775688f3bcf5ba
Olena Ageyeva <eageeva@hotmail.com>
2018-08-23 12:37:43 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-    #validates :email, uniqueness: true
-    #validates :username, uniqueness: true
-    #validates :username, :password, :email, presence: true
+    validates :email, uniqueness: true
+    validates :username, uniqueness: true
+    validates :username, :password, :email, presence: true
 
     has_secure_password
 
